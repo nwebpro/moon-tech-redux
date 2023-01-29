e98640f3496266cd6bf5fd0a2e31d69df7099a62
nwebpro <nwebpro.work@gmail.com>
2023-01-29 11:33:50 +0600

    action types and action creators

---
+++ src/redux/actionTypes/actionTypes.js
@@ -0,0 +1,9 @@
+// 1st way
+// export const actionTypes = {
+//     ADD_TO_CART: 'ADD_TO_CART',
+//     REMOVE_FROM_CART: 'REMOVE_FROM_CART', 
+// }
+
+// 2nd way
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
--- src/redux/reducers/productReducers.js
+++ src/redux/reducers/productReducers.js
@@ -1,9 +1,21 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes"
+
 const initialState = {
-    test: 'TEST'
+    cart: [],
 }
 
 const productReducers = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, action.payload],
+            }
+        case REMOVE_FROM_CART:
+            return {}
+        default:
+            return state
+    }
 }
 
 export default productReducers
